51107bbf9b92fec854cda8d090776f8597c6ebb6
Albert Lyu <albertworld@gmail.com>
2014-05-17 01:24:20 -0500

    Update shot chart background image to  dropbox link

--- app/assets/javascripts/shots.js
+++ app/assets/javascripts/shots.js
@@ -2,7 +2,7 @@ $(function() {
     $('#shotchart').highcharts({
         chart: {
             type: 'scatter',
-            plotBackgroundImage: '/assets/court.png'
+            plotBackgroundImage: 'https://dl-web.dropbox.com/get/court.png?_subject_uid=1177649&w=AAAwdf2A5Pzo3NY8iXJkzAtQ-myPY0KBXCJ_6U1FL14Khg'
         },
         title: {
             text: ''
