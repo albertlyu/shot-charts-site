feb1af5fd36176c40597bb0c46b5ec853a0cf5e6
Albert Lyu <albertworld@gmail.com>
2014-05-22 22:34:25 -0500

    Specify that heatmap.js must be precompiled in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ ShotCharts::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( heatmap.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
