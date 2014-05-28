640882ffbeb7fbeff539c6d2db912b0b7270948c
Albert Lyu <albertworld@gmail.com>
2014-05-27 22:36:45 -0500

    Removing search glyphicon for now, re-sized search bar for sm devices

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,5 @@ module ShotCharts
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.serve_static_assets = true
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
