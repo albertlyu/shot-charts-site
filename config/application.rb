1418390bcb6981fa4f7c04259fc291dc0eb531ad
Albert Lyu <albertworld@gmail.com>
2014-05-27 20:51:57 -0500

    Fixed search glyphicon

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module ShotCharts
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
